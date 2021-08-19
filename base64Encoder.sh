3d2d926a1e47951be1c8dca60df7876cbbbe7362
Shamodya <shamodya.hashantha80@gmail.com>
2021-08-19 08:21:09 +0530

    Adding comment for base64Encoder.sh file

--- base64Encoder.sh
+++ base64Encoder.sh
@@ -1,4 +1,6 @@
-echo -n "Enter your username: " 
+#This is a script for encoding username and password (base64 encoding)
+
+echo -n "Enter your username: "
 read username
 echo -n "Enter your password: " 
 read -s password
